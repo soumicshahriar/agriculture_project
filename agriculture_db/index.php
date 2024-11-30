f2f06526c883f79c92b607e8f1f3b2c8f7041d99
Goutom Roy <goutomg1234@gmail.com>
2024-11-30 13:14:21 +0600

    update

--- agriculture_db/index.php
+++ agriculture_db/index.php
@@ -1,7 +1,6 @@
 <?php
-// Database connection
-$conn = new mysqli("localhost", "root", "", "agriculture_product");
 
+include('../config/connect.php');
 // Check connection
 if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
