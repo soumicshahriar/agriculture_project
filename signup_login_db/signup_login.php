f5dde3238c55a5e9f4d1c8fc98520eca44ef1b0e
Goutom Roy <goutomg1234@gmail.com>
2024-12-19 23:53:33 +0600

    update signup.php

--- signup_login_db/signup_login.php
+++ signup_login_db/signup_login.php
@@ -18,15 +18,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $email = trim($_POST['email']);
         $password = trim($_POST['password']); 
 
-        // Check if email already exists
+        // Check if email already exists in customers
         $stmt = $conn->prepare("SELECT email FROM customers WHERE email = ?");
-        $stmt = $conn->prepare("SELECT email FROM employee WHERE email = ?");
-
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $stmt->store_result();
 
-        if ($stmt->num_rows > 0) {
+        // Check if email already exists in employee
+        $stmt_employee = $conn->prepare("SELECT email FROM employee WHERE email = ?");
+        $stmt_employee->bind_param("s", $email);
+        $stmt_employee->execute();
+        $stmt_employee->store_result();
+
+        if ($stmt->num_rows > 0 || $stmt_employee->num_rows > 0) {
             echo json_encode(["message" => "Error: The email address is already registered!"]);
         } else {
             // Insert the new user into the database
